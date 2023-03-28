5371101517572e8ff6f1c7661e94b2854e7972d4
MuzaffarAzizov <muzaffar2903@gmail.com>
2023-03-28 16:37:32 +0500

    first commit

---
+++ src/Data.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line
+export default [
+  {
+    id: 1,
+    title: "UI/UX дизайнер",
+    content: "Отдел разработки мобильного приложения",
+    name: "Алексей Щербаков",
+    img: "img1.png",
+  },
+  {
+    id: 2,
+    title: "Маркетолог",
+    content: "Отдел маркетинга",
+    name: "Floyd Miles",
+    img: "img2.png",
+  },
+  {
+    id: 3,
+    title: "Менеджер по продажам",
+    content: "Отдел продаж",
+    name: "Theresa Webb",
+    img: "img3.png",
+  },
+  {
+    id: 4,
+    title: "Freshers",
+    content: "Finance",
+    name: "Vanessa Johnson",
+    img: "img4.png",
+  },
+  {
+    id: 5,
+    title: "PHP Developer",
+    content: "Housekeepers",
+    name: "Kristin Watson",
+    img: "img5.png",
+  },
+];
